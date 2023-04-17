386299131bdfccd9b6e95c64a84615785e509b4f
ramadhanialfarizi <ramadhani.alfarizi@gmail.com>
2023-04-17 16:30:22 +0700

    create widget for movie pages

---
+++ lib/features/home/view/widget/movie/top_rate_movie.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import 'package:movie_app/core/utils/env.dart';
+import 'package:movie_app/features/home/model/movie/top_rate_movie_model.dart';
+
+class TopRateMovie extends StatefulWidget {
+  const TopRateMovie({super.key, this.topRateMovieModel});
+
+  final TopRateMovieModel? topRateMovieModel;
+
+  @override
+  State<TopRateMovie> createState() => _TopRateMovieState();
+}
+
+class _TopRateMovieState extends State<TopRateMovie> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 180,
+      width: double.infinity,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        shrinkWrap: true,
+        itemCount: widget.topRateMovieModel?.results.length,
+        itemBuilder: (context, index) {
+          return InkWell(
+            onTap: () {},
+            child: Container(
+              margin: const EdgeInsets.only(right: 12),
+              width: 150,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                image: DecorationImage(
+                  image: NetworkImage(
+                      '${imageURl}${widget.topRateMovieModel!.results[index].posterPath}'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(
+                  '${widget.topRateMovieModel?.results[index].title}',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/features/home/view/widget/movie/upcoming_movie.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/features/home/model/movie/upcoming_movie_model.dart';
+
+import '../../../../../core/utils/env.dart';
+
+class UpcomingMovie extends StatefulWidget {
+  const UpcomingMovie({super.key, required this.upcomingMovieModel});
+
+  final UpcomingMovieModel? upcomingMovieModel;
+
+  @override
+  State<UpcomingMovie> createState() => _UpcomingMovieState();
+}
+
+class _UpcomingMovieState extends State<UpcomingMovie> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 180,
+      width: double.infinity,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        shrinkWrap: true,
+        itemCount: widget.upcomingMovieModel?.results.length,
+        itemBuilder: (context, index) {
+          return InkWell(
+            onTap: () {},
+            child: Container(
+              margin: const EdgeInsets.only(right: 12),
+              width: 150,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                image: DecorationImage(
+                  image: NetworkImage(
+                      '$imageURl${widget.upcomingMovieModel?.results[index].posterPath}'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(
+                  widget.upcomingMovieModel!.results[index].title.toString(),
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
