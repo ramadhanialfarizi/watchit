f8ab283f9922a8357a217d28ba430a99042547d2
ramadhanialfarizi <ramadhani.alfarizi@gmail.com>
2023-04-17 16:26:29 +0700

    create environtment configuration

---
+++ lib/core/utils/env.dart
@@ -0,0 +1,5 @@
+var basedURL = 'https://api.themoviedb.org/3';
+var apiKey = '934f6ae90b94cf8745cbf08959893556';
+var apiReadAccesToken =
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MzRmNmFlOTBiOTRjZjg3NDVjYmYwODk1OTg5MzU1NiIsInN1YiI6IjY0MzdkYzIyMWQ1Mzg2MDA3N2E0NGVmNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IySmcJvp-Vwx4_G81YBUUK5oG5I_BIwQovGkNFOAxfA';
+var imageURl = 'https://image.tmdb.org/t/p/w500';
