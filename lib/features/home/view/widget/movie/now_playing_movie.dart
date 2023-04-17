83348d3fccd2b5a6e4785c4ac272bb67ba308091
ramadhanialfarizi <ramadhani.alfarizi@gmail.com>
2023-04-17 16:29:39 +0700

    create now playing movie model

---
+++ lib/features/home/view/widget/movie/now_playing_movie.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/features/home/model/movie/now_playing_movie_model.dart';
+
+import '../../../../../core/utils/env.dart';
+
+class NowPlayingMovie extends StatefulWidget {
+  const NowPlayingMovie({super.key, required this.nowPlayingMovieModel});
+
+  final NowPlayingMovieModel? nowPlayingMovieModel;
+
+  @override
+  State<NowPlayingMovie> createState() => _NowPlayingMovieState();
+}
+
+class _NowPlayingMovieState extends State<NowPlayingMovie> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 180,
+      width: double.infinity,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        shrinkWrap: true,
+        itemCount: widget.nowPlayingMovieModel?.results.length,
+        itemBuilder: (context, index) {
+          return InkWell(
+            onTap: () {},
+            child: Container(
+              margin: const EdgeInsets.only(right: 12),
+              width: 150,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                image: DecorationImage(
+                  image: NetworkImage(
+                      '$imageURl${widget.nowPlayingMovieModel?.results[index].posterPath}'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(
+                  '${widget.nowPlayingMovieModel?.results[index].title}',
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
